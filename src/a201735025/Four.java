7f179163fb914f73bfbac2524e25f0057ac0795f
minseokLeeStar <gksflaxkdns1@gmail.com>
2023-09-20 10:13:30 +0900

    four

---
+++ src/a201735025/Four.java
@@ -0,0 +1,8 @@
+package a201735025;
+
+public class Four {
+    
+    public static void print() {
+        System.out.println("four");
+    }
+}
--- src/a201735025/Main.java
+++ src/a201735025/Main.java
@@ -6,5 +6,6 @@ public class Main {
 		Two.print();
 		System.out.println("one 201735025");
 		Three.print();
+		Four.print();
 	}
 }
